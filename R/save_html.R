615a89e0f868d517c3251075cea14c270d46655f
luizfcp <luizfcpassos@hotmail.com>
2018-08-14 22:24:34 -0300

    save_html

--- R/save_html.R
+++ R/save_html.R
@@ -6,27 +6,22 @@
 #' @keywords html
 #' @export
 #' @examples
-#' arquivo <- head(iris) %>% regulartable() 
+#' arquivo <- head(iris) %>% regulartable()
 #' save_html("arquivo", "C:/Users/fulano/Desktop/arquivo.png")
 save_html <- function(filename, file) {
-  
+
   # create an Rmd file ----
   rmd_name <- tempfile(fileext = ".Rmd")
   cat(paste0("```{r echo=FALSE}\n", filename, "\n```"), file = rmd_name)
-  
+
   # render as an html file ----
   html_name <- tempfile(fileext = ".html")
   rmarkdown::render(rmd_name, output_format = "html_document", output_file = html_name )
-  
+
   # get a png from the html file with webshot ----
-  webshot::webshot(html_name, zoom = 2, file = file, 
+  webshot::webshot(html_name, zoom = 2, file = file,
                    selector = "body > div.container-fluid.main-container > div.tabwid > table")
 
   ## [credit] https://stackoverflow.com/questions/50225669/how-to-save-flextable-as-png-in-r
-  
+
 }
-
-##Ex:
-
-# arquivo <- head(iris) %>% regulartable() 
-# save_html("arquivo", "C:/Users/fulano/Desktop/arquivo.png")
